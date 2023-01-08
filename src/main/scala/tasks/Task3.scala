fad6a024ef05b0d998b53ad70eb92d5d553d12d8
Oksana <oksana.horlock@67bricks.com>
2023-01-08 14:22:07 +0000

    Day 3 part 2

--- src/main/scala/tasks/Task3.scala
+++ src/main/scala/tasks/Task3.scala
@@ -14,10 +14,10 @@ object Task3 extends App{
 
   def Task3_1(): Unit ={
     val total = inputLines.map{line=>
-      val first = line.substring(0, line.length /2)
-      val second = line.replace(first, "")
+      val firstRucksack = line.substring(0, line.length /2)
+      val secondRucksack = line diff firstRucksack
 
-      val commonElement = first.intersect(second).take(1)
+      val commonElement = firstRucksack.intersect(secondRucksack).take(1)
       alphabet.indexOf(commonElement) + 1
     }.sum
 
@@ -27,9 +27,8 @@ object Task3 extends App{
   def Task3_2(): Unit ={
     var total = 0
     for (i <- 0 to inputLines.length by 3){
-      val lastIndex = i+3
-      val groupOf3Rucksacks = inputLines.slice(i, lastIndex)
-      println(groupOf3Rucksacks)
+      val lastIndexIntheGroup = i+3
+      val groupOf3Rucksacks = inputLines.slice(i, lastIndexIntheGroup)
 
       val commonElementIn3Rucksacks = groupOf3Rucksacks.foldRight(groupOf3Rucksacks.take(1).toString())((x,y)=> {
         x.intersect(y)

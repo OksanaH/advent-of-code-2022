9494944eedca1df746b47440217999d4b5b36766
Oksana <oksana.horlock@67bricks.com>
2023-01-20 06:56:07 +0000

    Day 5 Task 2 test

--- src/test/scala/adventOfCodeTests/Task5Tests.scala
+++ src/test/scala/adventOfCodeTests/Task5Tests.scala
@@ -19,4 +19,16 @@ class Task5Tests extends AnyFunSuite {
     assert(Task5.stacks(1) == Stack("B", "A","W", "G", "H", "I"))
 
   }
+
+  test("Verify elements moved correctly by crane") {
+    Task5.stacks = Stack(mutable.Stack("A", "B", "C", "D", "E"), Stack("G", "H", "I"), Stack("W", "X", "Y", "Z"))
+    Task5.moveByCrane("move 3 from 3 to 1")
+    assert(Task5.stacks(0) == Stack("W", "X", "Y", "A", "B", "C", "D", "E"))
+    assert(Task5.stacks(2) == Stack("Z"))
+
+    Task5.moveByCrane("move 2 from 2 to 3")
+    assert(Task5.stacks(1) == Stack("I"))
+    assert(Task5.stacks(2) == Stack("G", "H", "Z"))
+
+  }
 }

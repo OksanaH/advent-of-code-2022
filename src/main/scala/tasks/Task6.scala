a30e3d4e06fcbc7b9997a3040f160b9206b24462
Oksana <oksana.horlock@67bricks.com>
2023-01-22 20:42:16 +0000

    Day 6 Task 2

--- src/main/scala/tasks/Task6.scala
+++ src/main/scala/tasks/Task6.scala
@@ -4,21 +4,25 @@ import scala.annotation.tailrec
 import scala.io.Source
 
 object Task6 extends App{
-  val markerSize = 4
+  val shortMarkerSize = 4
+  val longMarkerSize = 14
   val fileStream = getClass.getResourceAsStream("/resources/Day6_input.txt")
   var inputLines = Source.fromInputStream(fileStream).toSeq
 
   main()
 
   def main(): Unit ={
-    var totalCharacters = getStartOfPacket(0)
-    println(totalCharacters)
+    val charsBeforeShortMarker = getStartOfPacket(0, shortMarkerSize)
+    println(charsBeforeShortMarker)
+
+    val charsBeforeLongMarker = getStartOfPacket(0, longMarkerSize)
+    println(charsBeforeLongMarker)
   }
 
   @tailrec
-  def getStartOfPacket(startingIdx: Int): Int = {
+  def getStartOfPacket(startingIdx: Int, markerSize: Int): Int = {
     inputLines.drop(startingIdx).take(markerSize).distinct.length match {
-      case i if i < markerSize =>getStartOfPacket(startingIdx+1)
+      case i if i < markerSize =>getStartOfPacket(startingIdx+1, markerSize)
       case _ => return startingIdx+markerSize
     }
   }
--- src/test/scala/adventOfCodeTests/Task6Tests.scala
+++ src/test/scala/adventOfCodeTests/Task6Tests.scala
@@ -6,15 +6,22 @@ import tasks.Task6
 class Task6Tests extends AnyFunSuite {
 
   val input = List(
-    ("bvwbjplbgvbhsrlpgdmjqwftvncz", 5),
-    ("nppdvjthqldpwncqszvftbrmjlhg", 6),
-    ("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 11)
+    ("bvwbjplbgvbhsrlpgdmjqwftvncz", 5, 23),
+    ("nppdvjthqldpwncqszvftbrmjlhg", 6, 23),
+    ("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 11, 26)
   )
 
   input.foreach{i=>
-    test(s"Verify marker identified correctly for ${i._1}") {
+    test(s"Verify short marker identified correctly for ${i._1}") {
       Task6.inputLines = i._1.toSeq
-     assert(Task6.getStartOfPacket(0) === i._2)
+     assert(Task6.getStartOfPacket(0, Task6.shortMarkerSize ) === i._2)
+    }
+  }
+
+  input.foreach { i =>
+    test(s"Verify long marker identified correctly for ${i._1}") {
+      Task6.inputLines = i._1.toSeq
+      assert(Task6.getStartOfPacket(0, Task6.longMarkerSize) === i._3)
     }
   }
 

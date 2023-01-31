c420ea0130fd7e0c694ea2b494f4ad98b7412beb
Oksana <oksana.horlock@67bricks.com>
2023-01-31 22:49:48 +0000

    Day 7 WIP

--- src/main/scala/tasks/Task7.scala
+++ src/main/scala/tasks/Task7.scala
@@ -1,78 +1,53 @@
 package tasks
 
-import scala.annotation.tailrec
-import scala.collection.mutable.ArrayBuffer
 import scala.io.Source
 import scala.util.matching.Regex
 
 object Task7 extends App{
 
   val fileStream = getClass.getResourceAsStream("/resources/Day7_input.txt")
-  var inputLines = Source.fromInputStream(fileStream).getLines.toList.zipWithIndex
-  var fileSystem = ArrayBuffer[ElfDeviceFileSystemFile]()
-
+  var inputLines = Source.fromInputStream(fileStream).getLines.toList
+  var sizes: scala.collection.mutable.Map[String, Int] =  scala.collection.mutable.Map("/home"-> 0)
   main()
 
   def main(): Unit ={
-    processLog(inputLines.headOption.get, "/")
-    println(findFoldersOfSize(1000000))
-  }
+    processLog()
 
+    println(sizes.filter(_._2<100000).map(_._2).sum)
+  }
   def findFoldersOfSize(size: Long): Long = {
-    var total:Long= 0
-    var allPaths = fileSystem.map(file=>{
-      file.path.split("/").filterNot(_.isEmpty).toList
-    }).flatten.distinct
-    println(allPaths.toString)
-    allPaths.foreach(p=>{
-      val folderSize = calculateFolderSize(p)
-      if (folderSize<=size)
-        total += folderSize
-    })
-    total
+    sizes.filter(_._2<100000).map(_._2).sum
   }
-
-  @tailrec
-  def processLog(line: (String,Int), curDir:String): Unit ={
-   println(s"Got $line")
-    var inDirectory = curDir
-    line match {
-      case (l, _) if l.contains("cd /") => None
-      case (l, _) if l.contains("cd") && (!l.contains("/") && !l.contains("..")) => {
-        inDirectory = curDir match {
-          case "/"=>curDir.concat(l.replace("$ cd", "").trim)
-          case _ => curDir.concat(l.replace("$ cd ", "/").trim)
+  def processLog(): Unit ={
+    var currentPath = ""
+    inputLines foreach(line =>{
+      line match {
+        case line if line.contains("ls") => None
+        case line if line.contains("cd ..") => {
+            currentPath = currentPath.substring(0, currentPath.lastIndexOf("/"))
+            println(s" going back into $currentPath, line = $line")
         }
-      }
-      case (l, _) if l.contains("cd ..") => {
-        inDirectory = goUpAFolder(inDirectory) // inDirectory.split("/").reverse.take(1).mkString
-        println(s" new inDir = $inDirectory")
-      }
-      case (l, _) if (new Regex("\\d{1,}\\s[a-zA-z.]{1,}").findAllIn(l)).length == 1 => { //line._1.forall(Character.isDigit) && !line._1.contains("dir") => {
-        val size =  (new Regex("\\d{1,}") findFirstIn  l).map(_.toLong).get
-        val name = l.replace(size.toString(), "").trim
-        println(ElfDeviceFileSystemFile(name, curDir ,size).toString)
-        fileSystem += ElfDeviceFileSystemFile(name, curDir ,size)
-      }
-      case (l, _) if l.contains ("ls") => None
-      case _ => None
-    }
-    if (line._2 + 1 < inputLines.length) {
-      processLog(inputLines(line._2 + 1), inDirectory)
-    }
-  }
+        case line if line.contains("$ cd /") => currentPath = "/home"
+        case line if line.contains("$ cd") && (!line.contains("/") && !line.contains("..")) => {
+          println(s" current directory to cd into: $currentPath, line = $line")
+          //if (currentPath == "")
+           // currentPath = "/home"
+          currentPath = currentPath.concat("/").concat(line.split(" ").last)
+          sizes(currentPath)= 0
+        }
 
-  def goUpAFolder(path:String): String ={
-    path.count(_ == '/') match {
-      case 1 => return "/"
-      case 0 => throw new Exception("Wrong")
-      case _ => path.substring(0, path.lastIndexOf("/"))
-    }
-  }
+        case line if line.exists(_.isDigit) => {
+          val size = line.split(" ").toList.head
+          var temp = currentPath
+          while (temp != "") {
+            sizes(temp) += size.toInt
+            temp = temp.substring(0, temp.lastIndexOf("/"))
+          }
+        }
+        case _ => None
+      }
+    })
 
-  def calculateFolderSize(folderName: String): Long = {
-      fileSystem.filter(_.path.contains(s"/$folderName")).map(f => f.size).sum
+    println(sizes)
   }
-
-  case class ElfDeviceFileSystemFile(var name: String, var path: String, var size: Long)
 }
--- src/test/scala/adventOfCodeTests/Day7Tests.scala
+++ src/test/scala/adventOfCodeTests/Day7Tests.scala
@@ -2,83 +2,67 @@ package adventOfCodeTests
 
 import org.scalatest.funsuite.AnyFunSuite
 import tasks.Task7
-import tasks.Task7.ElfDeviceFileSystemFile
-
-import scala.collection.mutable.ArrayBuffer
 import scala.io.Source
 
 class Day7Tests extends AnyFunSuite {
+  Task7.sizes = scala.collection.mutable.Map("/home" -> 0)
 
-  val fileSystem = ArrayBuffer(
-    ElfDeviceFileSystemFile("a", "/home/data", 100),
-    ElfDeviceFileSystemFile("b", "/home/data", 200),
-    ElfDeviceFileSystemFile("c", "/var/opt", 300),
-    ElfDeviceFileSystemFile("d", "/var/opt/cache", 400),
-    ElfDeviceFileSystemFile("e", "/var/opt/cache/log", 500),
-    ElfDeviceFileSystemFile("f", "/var/opt/cache/log/20202", 600),
-    ElfDeviceFileSystemFile("g", "/var/opt/cache/log/20203", 600),
-  )
-  val dirSizes = Seq(("/home", 300), ("/home/data", 300), ("/var", 2400), ("/var/opt/cache", 2100), ("/var/opt/cache/log", 1700))
+  test("Ignore correctly") {
+    val input = List("$ cd /", "$ ls")
+    val fileStream = getClass.getResourceAsStream("/Day7_testData.txt")
+    Task7.inputLines = List("$ cd /", "$ ls")
 
-  dirSizes.foreach { dir =>
-    test(s"Verify folder size calculated correctly for ${dir._1}") {
-      assert(Task7.calculateFolderSize(dir._1) == dir._2)
-    }
+    Task7.processLog()
+
+    val expected = Task7.sizes
+    assert(Task7.sizes.size == 1)
+    assert(Task7.sizes.equals(expected))
+
   }
 
-  test("Verify you can go up a folder correctly") {
-    val data = Seq(("/a/b/c/d", "/a/b/c"), ("/a/b/c", "/a/b"), ("/a/b", "/a"), ("/a", "/"))
-    data.foreach { d =>
-      assert(Task7.goUpAFolder(d._1) === d._2)
+  test("Go into folder correctly") {
+    val fileStream = getClass.getResourceAsStream("/Day7_testData.txt")
+    Task7.inputLines = List(
+      "$ cd /",
+      "$ ls",
+      "dir some",
+      "dir test",
+      "11 sd.as",
+      "$ cd some",
+      "$ ls",
+      "dir other",
+      "11 sd.as",
+      "$ cd other",
+      "$ ls",
+      "dir another",
+      "11 sd.as",
+      "$ cd another",
+      "$ cd ..",
+      "$ cd ..",
+      "$ cd ..",
+      "$ cd test"
+    )
+    Task7.processLog()
+
+    val expected = scala.collection.mutable.Map("/home" -> 0, "/home/some" -> 0, "/home/some/other" -> 0,"/home/some/other/another" -> 0, "/home/test" -> 0)
+    assert(Task7.sizes.size == 5)
+    Task7.sizes foreach { case (folder, size) =>
+      assert(expected.exists(e => e._1 == folder))
     }
   }
 
   test("Verify log parsed correctly") {
-    val fileStream = getClass.getResourceAsStream("/Day7_testData.txt")
-    var inputLines = Source.fromInputStream(fileStream).getLines.toList.zipWithIndex
-    Task7.inputLines = inputLines
-    Task7.fileSystem = ArrayBuffer[ElfDeviceFileSystemFile]()
 
-    val expected = ArrayBuffer(
-      ElfDeviceFileSystemFile("b.txt", "/", 14848514),
-      ElfDeviceFileSystemFile("c.dat", "/", 8504156),
-      ElfDeviceFileSystemFile("f", "/a", 29116),
-      ElfDeviceFileSystemFile("g", "/a", 2557),
-      ElfDeviceFileSystemFile("h.lst", "/a", 62596),
-      ElfDeviceFileSystemFile("i", "/a/e", 584),
-      ElfDeviceFileSystemFile("d.log", "/d", 8033020),
-      ElfDeviceFileSystemFile("j", "/d", 4060174),
-      ElfDeviceFileSystemFile("d.ext", "/d", 5626152),
-      ElfDeviceFileSystemFile("k", "/d", 7214296)
-    )
-    Task7.processLog(inputLines.headOption.get, "/")
+  val fileStream = getClass.getResourceAsStream("/Day7_testData.txt")
+    Task7.inputLines = Source.fromInputStream(fileStream).getLines.toList
+    Task7.processLog()
 
-    println(Task7.fileSystem)
+    val expected = scala.collection.mutable.Map("/home"->99, "/home/a"->19, "/home/a/e"->8,"/home/d"->50)
+    println(Task7.sizes)
 
-    Task7.fileSystem foreach {file =>{
-        assert(expected.contains(file))
+    Task7.sizes foreach {case (folder, size) =>{
+        assert(expected.exists(e=>e._1== folder && e._2==size))
       }
   }}
 
-  test("Verify folders with size max 100000 are identified correctly") {
-    val fileStream = getClass.getResourceAsStream("/Day7_testData.txt")
-    var inputLines = Source.fromInputStream(fileStream).getLines.toList.zipWithIndex
-    Task7.inputLines = inputLines
-    Task7.fileSystem = ArrayBuffer[ElfDeviceFileSystemFile]()
-
-    val files = ArrayBuffer(
-      ElfDeviceFileSystemFile("b.txt", "/", 14848514),
-      ElfDeviceFileSystemFile("c.dat", "/", 8504156),
-      ElfDeviceFileSystemFile("f", "/a", 29116),
-      ElfDeviceFileSystemFile("g", "/a", 2557),
-      ElfDeviceFileSystemFile("h.lst", "/a", 62596),
-      ElfDeviceFileSystemFile("i", "/a/e", 584),
-      ElfDeviceFileSystemFile("d.log", "/d", 8033020),
-      ElfDeviceFileSystemFile("j", "/d", 4060174),
-      ElfDeviceFileSystemFile("d.ext", "/d", 5626152),
-      ElfDeviceFileSystemFile("k", "/d", 7214296)
-    )
-    Task7.fileSystem = files
-    assert (Task7.findFoldersOfSize(100000) == 95437)
-  }
 }

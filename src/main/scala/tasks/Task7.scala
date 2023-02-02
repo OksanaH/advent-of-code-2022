6750a9c3dc7956a87f303cc0c770032d7145f77b
Oksana <oksana.horlock@67bricks.com>
2023-02-02 21:43:19 +0000

    Finish Task 7 Part 2

--- src/main/scala/tasks/Task7.scala
+++ src/main/scala/tasks/Task7.scala
@@ -1,51 +1,50 @@
 package tasks
 
 import scala.io.Source
-import scala.util.matching.Regex
 
 object Task7 extends App{
 
   val fileStream = getClass.getResourceAsStream("/resources/Day7_input.txt")
   var inputLines = Source.fromInputStream(fileStream).getLines.toList
   var sizes: scala.collection.mutable.Map[String, Int] =  scala.collection.mutable.Map("/home"-> 0)
+
   main()
 
   def main(): Unit ={
     processLog()
     println(sizes.filter(_._2<100000).map(_._2).sum)
+
+    //Part 2
+    val spaceNeeded:Int = 30000000-(70000000-sizes("/home"))
+    val folderToDelete = sizes.filter(s=>spaceNeeded<=s._2).map(_._2).min
+    println(folderToDelete)
   }
   def processLog(): Unit ={
-    var currentPath = ""
-    inputLines foreach(line =>{
+    var currentPath = "/home"
+    inputLines.drop(1) foreach(line =>{
       line match {
-        case line if line.contains("$ ls") || line.startsWith("dir ")=> None
-        case line if line.contains("$ cd ..") => {
-          if (currentPath!= "/home") {
-            currentPath = currentPath.substring(0, currentPath.lastIndexOf("/"))
+        case line if line.contains("$ cd") =>
+          line match {
+            case line if line.contains("..") =>
+              if (currentPath != "/home")
+                 currentPath = currentPath.substring(0, currentPath.lastIndexOf("/"))
+
+              if (currentPath=="/") currentPath = "/home"
+            case _ =>
+              val dir = line.split(" ").last
+              currentPath = s"$currentPath/$dir"
+              sizes.put(currentPath, 0)
           }
-        }
-        case line if line.contains("$ cd /")  => currentPath = "/home"
-        case line if line.contains("$ cd") && (!line.contains("/") && !line.contains("..")) => {
-          val dir = line.split(" ").last
-          currentPath = s"$currentPath/$dir"
+        case line if line.contains("$ ls") || line.startsWith("dir ") => None
 
-          println(s"Went into dir $dir, current dir is $currentPath")
-          sizes.put(currentPath, 0)
-          println(sizes)
-        }
-
-        case line if line.exists(_.isDigit) => {
+        case line if line.exists(_.isDigit) =>
           val size = line.split(" ").toList.head
           var temp = currentPath
           while (temp != "") {
-            println(s"Updating folder $temp; current directory is $currentPath")
             sizes(temp) += size.toInt
             temp = temp.substring(0, temp.lastIndexOf("/"))
           }
-        }
-        case _ => {
-          println(s"no match $line")
-        }
+        case _ => None
       }
     })
   }
